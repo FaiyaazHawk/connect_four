0d0911c80eae55cdd3371e5695eac680c48a69b7
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-03-04 06:08:48 -0500

    created and tested board init

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,15 @@
 require_relative "../board.rb"
 
 describe Board do
-    
+    context 'it creates a grid 7 col wide by 6 rows tall' do
+        it "is 7 columns wide" do
+            board = Board.new
+            expect(board.grid[0].length).to eq(7) 
+        end
+
+        it 'is 6 rows tall' do
+            board = Board.new
+            expect(board.grid.length).to eq(6)
+        end
+    end
+end
